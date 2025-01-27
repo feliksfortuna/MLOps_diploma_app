5363e791f739b60bd20ab207b99385bcadfe065c
feliksfortuna <feliks.fortuna@gmail.com>
2025-01-27 16:17:19 +0100

    Added new devops model, debugging 3

--- devops/model_def.py
+++ devops/model_def.py
@@ -1,4 +1,5 @@
-from torch import nn
+import torch
+import torch.nn as nn
 
 class RaceRegressionModel(nn.Module):
     def __init__(self, input_size, hidden_size=128):
@@ -12,3 +13,9 @@ class RaceRegressionModel(nn.Module):
         out = self.relu(out)
         out = self.fc2(out)
         return out.squeeze()
+    
+    def predict(self, X):
+        self.eval()
+        with torch.no_grad():
+            X = torch.tensor(X, dtype=torch.float32)
+            return self.forward(X).numpy()
